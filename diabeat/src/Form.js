2c54c93b46ddbf86e51f9989ea3b3472d8936bb1
Robert Le <robertle982@gmail.com>
2024-04-28 07:31:54 -0400

    working form

--- diabeat/src/Form.js
+++ diabeat/src/Form.js
@@ -1,13 +1,30 @@
-function Form(){
+import {useState} from 'react';
 
+
+function Form(){
+const [form,  setForm] = useState({name: "", email: ""});
     const handleSubmit = (event)=>{
         event.preventDefault();
-        console.log("form submitted");
 
+        console.log("form submitted");
+        console.log(form);
     };
+
+    const onChange = (event)=>{
+        console.log("changed input field");
+        const name = event.target.name;
+        const value = event.target.value;
+
+    console.log(`event.target.name: ${name}, event.target.value: ${value}`);
+
+    setForm({...form, [name]: value});
+    }
+
     return(
        <form onSubmit = {handleSubmit}>
-        <button type = "submit">Submit Form</button>
+        <input type = "text" name = "name"  onChange={onChange} placeholder = "Name (e.g. John)" />    
+        <input type = "email" name = "email"  onChange={onChange} placeholder = "email (john doe@example.com)" />    
+          <button type = "submit">Submit Form</button>
        </form>
     )
 }
