941287db339728264ddf0f4977dbde17238770bd
Robert Le <robertle982@gmail.com>
2024-05-01 08:29:10 -0400

    fixing api

--- backend/DSmodel.py
+++ backend/DSmodel.py
@@ -16,7 +16,7 @@ import pandas as pd
 import pickle 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from flask import Flask, render_template, request
+
 
 # Read the dataset (CSV file) into a pandas DataFrame
 df = pd.read_csv('diabetes.csv')
@@ -41,44 +41,7 @@ knn.fit(X_train, y_train)
 
 #pickle data to serialize the file then deserialize it back (save model as pickle file)
 
-model = pickle.load(open("example_weights_knn.pkl", "rb"))
-
-
-@app.route('/')
-def use_template():
-    return render_template("index.html") #frontend calling
-
-@app.route('/predict',methods=['POST', 'GET'])
-def predict():
-    input_1 = request.form['1']
-    input_2 = request.form['2']
-    input_3 = request.form['3']
-    input_4 = request.form['4']
-    input_5 = request.form['5']
-    input_6 = request.form['6']
-    input_7 = request.form['7']
-    input_8 = request.form['8']
-
-    # Setup input data as a DataFrame
-    setup_df = pd.DataFrame([[input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8]])
-
-    # Make predictions using the loaded model
-    diabetes_prediction = model.predict_proba(setup_df)
-    output = '{0:.{1}f}'.format(diabetes_prediction[0][1],2)
-    output = str(float(output)*100) + '%'
-    
-
-    # Decide prediction based on probability threshold
-    if output >str(0.5):
-        return render_template('result.html', pred=f 'You have the following chance of diabetes based on our KNN model is {output} ')
-    else:
-     return render_template('result.html', pred=f 'you have a low chance of diabetes which is currently considered good your probability is {output} ')
-    
-if __name__ == '__main__':
-    # app.run(host = '0.0.0.0', port = 80)
-    app.run(debug=True) #default flask port
-
-
+pickle.dump(knn, open('example_weights_knn.pkl', "wb"))
 
 
 
--- backend/app.py
+++ backend/app.py
@@ -10,21 +10,39 @@ model = pickle.load(open("example_weights_knn.pkl", "rb"))
 
 
 
-
-@app.route('/') #app route tell Flask 
+@app.route('/')
 def use_template():
-    return render_template("index.html") # connect to frontend
+    return render_template("index.html") #frontend calling
 
-# def greeting():
-#     return 'Flask is awesome'
-# API
- @app.route('/predict', methods = ['POST','GET'])
- def predict():
-   input_1 = request.form['1']
-input_2 = request.form['2']
-input_3 = request.form['3']
-input_4 = request.form['4']
-input_5 = request.form['5']
-input_6 = request.form['6']
-input_7 = request.form['7']
-input_8 = request.form['8']
+@app.route('/predict',methods=['POST', 'GET'])
+def predict():
+    input_1 = request.form['1']
+    input_2 = request.form['2']
+    input_3 = request.form['3']
+    input_4 = request.form['4']
+    input_5 = request.form['5']
+    input_6 = request.form['6']
+    input_7 = request.form['7']
+    input_8 = request.form['8']
+
+    # Setup input data as a DataFrame
+    setup_df = pd.DataFrame([[input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8]])
+
+    # Make predictions using the loaded model
+    diabetes_prediction = model.predict_proba(setup_df)
+    output = '{0:.{1}f}'.format(diabetes_prediction[0][1],2)
+    output = str(float(output)*100) + '%'
+    
+
+    # Decide prediction based on probability threshold
+    if output >str(0.5):
+        return render_template('result.html', pred=f 'You have the following chance of diabetes based on our KNN model is {output} ')
+    else:
+     return render_template('result.html', pred=f 'you have a low chance of diabetes which is currently considered good your probability is {output} ')
+    
+if __name__ == '__main__':
+    # app.run(host = '0.0.0.0', port = 80)
+    app.run(debug=True) #default flask port
+
+
+
