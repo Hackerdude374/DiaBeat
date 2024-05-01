2c2a3bb2532f764b2e8d91ccd40aa02397a19d28
Robert Le <robertle982@gmail.com>
2024-05-01 19:56:39 -0400

    working html

--- backend/app.py
+++ backend/app.py
@@ -40,9 +40,17 @@ def predict():
     else:
      return render_template('result.html', pred=f'you have a low chance of diabetes which is currently considered good your probability is {output} ')
     
-if __name__ == '__main__':
+    
+    
+app = Flask(__name__)
+
+@app.route('/')
+
+def index(): 
+    
+    if __name__ == '__main__':
     # app.run(host = '0.0.0.0', port = 80)
-    app.run(debug=True) #default flask port
+        app.run(debug=True) #default flask port
 
 
 
