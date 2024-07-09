9c9abba9b25e37b127e0af756aaba35d6a54974c
Robert Le <robertle982@gmail.com>
2024-07-08 23:02:18 -0400

    trying ts particles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,104 @@
+import React from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import diabeatLogo from './DiaBeatLogo.png';
-
 import './App.css';
 import Form from './Form';
+
 function App() {
-  
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {};
+
   return (
     <div className="App">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#ff0000",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#aa0000",
+            },
+            links: {
+              color: "#aa0000",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "right",
+              enable: true,
+              outMode: "out",
+              random: false,
+              speed: 1,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 3,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <header className="App-header">
         <img src={diabeatLogo} className="App-logo" alt="DiaBeatLogo" />
-
-      <Form />
-
+        <Form />
       </header>
     </div>
   );
